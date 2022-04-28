04e9428760cfc6d878c9886cf303f0e3bc385fd0
busegul-ozkaya <85351329+busegul-ozkaya@users.noreply.github.com>
2022-04-28 23:10:24 +0300

    Add files via upload

---
+++ myFirstLabScript.py
@@ -0,0 +1,25 @@
+name = input("what is your name?\n"">>")
+print(name)
+
+print("hello", name,"\n")
+
+id = int(input("what is your student id?\n"">>"))
+print("your id is",id)
+
+print("name type ",type(name))
+print("id type ",type(id))
+
+
+number1 = input("enter first number\n"">>")
+number2 = input("enter second number\n"">>")
+
+sum = int(number1)+int(number2)
+print("summation",sum)
+
+diff = int(number1)-int(number2)
+print("difference",diff)
+
+prod = int(number1)*int(number2)
+print("prod",prod)
+
+print("*\n**\n***")

3927baab9ccf3ef930383cb91fdec2fe3a42e5c5
busegul-ozkaya <85351329+busegul-ozkaya@users.noreply.github.com>
2022-04-28 23:18:37 +0300

    Add files via upload

---
+++ myFirstC++Script.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int id;
+    string name;
+    cout<<"enter your name: "<<endl;
+    cin>>name;
+    cout<<"hello "<<name<<endl;
+
+    cout<<"enter your id: "<<endl;
+    cin>>id;
+    cout<<"your id is: "<<id<<endl;
+
+    int firstNum;
+    int secondNum;
+    int sum;
+    int diff;
+    int prod;
+
+    cout<<"enter first number: "<<endl;
+    cin>>firstNum;
+    cout<<"enter second number: "<<endl;
+    cin>>secondNum;
+    cout<<"summation is "<<firstNum+secondNum<<endl;
+    cout<<"difference is "<<firstNum-secondNum<<endl;
+    cout<<"product is "<<firstNum*secondNum<<endl;
+
+    cout<<"*"<<endl<<"**"<<endl<<"***";
+}
